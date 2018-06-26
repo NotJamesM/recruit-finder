d2c3afe173926d2176b53450b1b2bb656bbde044
NotJamesM <merringtonjames@gmail.com>
2018-06-27 00:07:17 +0100

    added example of handling events, WIP

--- RecruitFinder.lua
+++ RecruitFinder.lua
@@ -1,5 +1,11 @@
-function RecruitFinder:OnLoad()
+local frame = CreateFrame("FRAME", "RecruitFinderFrame");
+frame:RegisterEvent("PLAYER_ENTERING_WORLD");
+local function eventHandler(self, event, ...)
+  print("HELLO WORLD");
+end
+frame:SetScript("OnEvent", eventHandler);
 
+function RecruitFinder_OnLoad(self)
   DEFAULT_CHAT_FRAME:AddMessage("RecruitFinder LOADED!");
   print("RecruitFinder LOADED!");
   self:RegisterEvent("GROUP_JOINED")
@@ -7,12 +13,12 @@ function RecruitFinder:OnLoad()
 
 end
 
-function RecruitFinder:GROUP_JOINED()
+function RecruitFinder_GROUP_JOINED(self)
   print("GROUP_JOINED");
   DEFAULT_CHAT_FRAME:AddMessage("GROUP_JOINED");
 end
 
-function RecruitFinder:GROUP_ROSTER_CHANGED()
+function RecruitFinder_GROUP_ROSTER_CHANGED(self)
   print("roster changed");
   DEFAULT_CHAT_FRAME:AddMessage("GROUP_ROSTER_CHANGED");
 end
